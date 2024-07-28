8599e51f3212c17aaa9b5e7153bde5820cc3d8fe
PY <pheyying098@gmail.com>
2024-07-28 22:49:26 +0800

    Completed NavBar task

--- aeon-web/src/app/App.tsx
+++ aeon-web/src/app/App.tsx
@@ -1,34 +1,62 @@
-import { useState } from "react";
-import reactLogo from "../../src/assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+interface NavBarItemProps {
+  path: string;
+  title: string;
+}
+
+function NavBarItem({ path, title }: NavBarItemProps) {
+  return (
+    <li>
+      <a href={path}>{title}</a>
+    </li>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const navBarItems: NavBarItemProps[] = [
+    {
+      path: "/",
+      title: "Showcase",
+    },
+    {
+      path: "/",
+      title: "Docs",
+    },
+    {
+      path: "/",
+      title: "Blog",
+    },
+    {
+      path: "/",
+      title: "Analytics",
+    },
+    {
+      path: "/",
+      title: "Template",
+    },
+    {
+      path: "/",
+      title: "Enterprise",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">AEON</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <ul className="navbar-links">
+        {navBarItems.map((item) => (
+          <NavBarItem path={item.path} title={item.title} />
+        ))}
+      </ul>
+
+      <div className="navbar-search">
+        <input type="text" placeholder="Search documentation..." />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </nav>
   );
 }
 
