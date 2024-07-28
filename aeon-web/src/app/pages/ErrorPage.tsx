407751c0d4dd812255b285da9ef69a826d709238
PY <pheyying098@gmail.com>
2024-07-28 23:41:36 +0800

    Update Error page

--- aeon-web/src/app/pages/ErrorPage.tsx
+++ aeon-web/src/app/pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p style={{ color: "black" }}>Sorry, an unexpected error has occurred.</p>
     </div>
   );
 }
