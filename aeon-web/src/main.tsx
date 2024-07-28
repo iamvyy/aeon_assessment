e53cccb594e3e7872420ba43f13bf4bf4fa24f16
PY <pheyying098@gmail.com>
2024-07-28 18:01:58 +0800

    Setup react-vite project

---
+++ aeon-web/src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App.tsx";
+import "../src/app/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from "./app/pages/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
